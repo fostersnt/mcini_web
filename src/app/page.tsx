5aa17b3c54949ff2a4fc8978ff24abc50ced3d0b
Foster Amponsah Asante <fostersnt@gmail.com>
2024-10-21 11:58:55 +0000

    Added a route link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import styles from "./page.module.css";
 import UserIndexPage from "./users/page";
+import Link from 'next/link'
 
-const Home: React.FC = () => {
+const Home: React.FC = (): JSX.Element => {
   const userName: string = 'John Doe';
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <UserIndexPage myName={userName} />
+      <Link href="/users">CLICK HERE TO VISIT USERS PAGE</Link>
+        <h1>
+        </h1>
       </main>
       <footer className={styles.footer}></footer>
     </div>
